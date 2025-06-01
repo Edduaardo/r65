88eb53467970657086cb54b0a257b3e30fb44218
Edduaardo <eduhmz86@gmail.com>
2025-06-01 12:38:39 -0300

    Move `WIN_LOCK_PIN` LED logic to `process_record_user()`

--- rk/r65/r65.c
+++ rk/r65/r65.c
@@ -16,13 +16,3 @@ void keyboard_pre_init_kb(void) {
 
     keyboard_pre_init_user();
 }
-
-void housekeeping_task_kb(void){
-
-    if(keymap_config.no_gui)
-    {
-        gpio_write_pin_low(LED_WIN_LOCK_PIN);
-    } else {
-        gpio_write_pin_high(LED_WIN_LOCK_PIN);
-    }
-}
